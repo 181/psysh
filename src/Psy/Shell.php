6601d00f03a1980ab95937e4f65e64a3d48d0c38
Nate Abele <nate.abele@gmail.com>
2013-09-29 20:42:08 -0400

    Initial (very rough) draft of live code reloading. I kind of hate it, but it works.

--- src/Psy/Shell.php
+++ src/Psy/Shell.php
@@ -65,6 +65,7 @@ class Shell extends Application
     private $outputWantsNewline = false;
     private $completion;
     private $tabCompletionMatchers = array();
+    private $listeners;
 
     /**
      * Create a new Psy Shell.
@@ -73,13 +74,14 @@ class Shell extends Application
      */
     public function __construct(Configuration $config = null)
     {
-        $this->config   = $config ?: new Configuration();
-        $this->cleaner  = $this->config->getCodeCleaner();
-        $this->loop     = $this->config->getLoop();
-        $this->context  = new Context();
-        $this->includes = array();
-        $this->readline = $this->config->getReadline();
+        $this->config      = $config ?: new Configuration();
+        $this->cleaner     = $this->config->getCodeCleaner();
+        $this->loop        = $this->config->getLoop();
+        $this->context     = new Context();
+        $this->includes    = array();
+        $this->readline    = $this->config->getReadline();
         $this->inputBuffer = array();
+        $this->listeners   = $this->getDefaultListeners();
 
         parent::__construct('Psy Shell', self::VERSION);
 
@@ -244,6 +246,20 @@ class Shell extends Application
     }
 
     /**
+     * Gets the default command loop listeners.
+     *
+     * @return array An array of Listener instances
+     */
+    protected function getDefaultListeners()
+    {
+        return array_filter(array(
+            new Listener\Reloader(),
+        ), function ($listener) {
+            return $listener->enabled();
+        });
+    }
+
+    /**
      * @param array $matchers
      */
     public function addTabCompletionMatchers(array $matchers)
@@ -380,7 +396,7 @@ class Shell extends Application
      */
     public function beforeLoop()
     {
-        $this->loop->beforeLoop();
+        $this->loop->beforeLoop($this);
     }
 
     /**
@@ -390,7 +406,21 @@ class Shell extends Application
      */
     public function afterLoop()
     {
-        $this->loop->afterLoop();
+        $this->loop->afterLoop($this);
+    }
+
+    /**
+     * Pass next command to listeners.
+     *
+     * @see Loop::run
+     */
+    public function onExecute($command)
+    {
+        foreach ($this->listeners as $listener) {
+            $listener->onExecute($this, $command);
+        }
+
+        return $command;
     }
 
     /**
